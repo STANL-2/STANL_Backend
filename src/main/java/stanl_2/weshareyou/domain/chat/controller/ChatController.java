c1e10222bdcb6cf862b00b743031ea8730dea374
giuseog <giuseog@gmail.com>
2024-10-25 12:33:08 +0900

    feat: 알림 기능 추가중

--- src/main/java/stanl_2/weshareyou/domain/chat/controller/ChatController.java
+++ src/main/java/stanl_2/weshareyou/domain/chat/controller/ChatController.java
@@ -26,8 +26,22 @@ public class ChatController {
             chatRoomMessageService.addMessageToRoom(message.getRoomId(), message.getSender(), message.getMessage());
             // 구독 경로를 하나 더 추가하면 알림에 뜨게 할 수 있을 듯?
             messagingTemplate.convertAndSend("/sub/" + roomId, message);
+
+            //알림 메시지 전송
+            ChatMessage notificationMessage = createNotificationMessage(message);
+            messagingTemplate.convertAndSend("/sub/notifications/" + roomId, notificationMessage);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private ChatMessage createNotificationMessage(ChatMessage message){
+        ChatMessage notificationMessage = new ChatMessage();
+
+        notificationMessage.setSendTime(message.getSendTime());
+        notificationMessage.setSender(message.getSender());
+        notificationMessage.setMessage(message.getMessage());
+        return notificationMessage;
     }
 }
